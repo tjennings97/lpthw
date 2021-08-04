9e0c6b796df2032bf60d501fc8b7e7bbf53c5bf1
tjennings97 <tamara.jennings97@gmail.com>
2021-08-04 19:30:26 -0400

    edit ex15, add ex16 & ex17

--- ex15.py
+++ ex15.py
@@ -1,7 +1,11 @@
+# import library
+# sys is a package, argv is the feature in that package
 from sys import argv
 
+# set variables equal to command line arguments
 script, filename = argv
 
+# open the given file; returns a file object
 txt = open(filename)
 
 # use argv for filename
@@ -15,3 +19,7 @@ file_again = input("> ")
 txt_again = open(file_again)
 
 print(txt_again.read())
+
+# close files, it's important to do so when done with them
+txt.close()
+txt_again.close()
