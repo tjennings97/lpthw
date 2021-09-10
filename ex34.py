d29201c7b2b7e76ae79b100cae4fc354fb2ff651
tjennings97 <tamara.jennings97@gmail.com>
2021-09-10 17:16:00 -0400

    add ex34

---
+++ ex34.py
@@ -0,0 +1,12 @@
+animals = ['bear', 'python3.6', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print(f"""
+1. The animal at 1 is a {animals[1]}.
+2. The third (3rd) animal is at 2 and is a {animals[2]}.
+3. The first (1st) animal is at 0 and is a {animals[0]}.
+4. The animal at 3 is a {animals[2]}.
+5. The fifth (5th) animal is at 4 and is a {animals[4]}.
+6. The animal at 2 is a {animals[1]}.
+7. The sixth (6th) animal is at 5 and is a {animals[5]}.
+8. The animal at 4 is a {animals[3]}.
+""")
